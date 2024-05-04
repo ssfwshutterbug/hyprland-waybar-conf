2bf70505d6b9eb7fc1e1a54a97c7f9cff9a2af22
mandalorian <ssfwshutterbug@gmail.com>
2024-05-04 22:57:59 +0800

    modify volume script more stable

--- scripts/getgpufan.sh
+++ scripts/getgpufan.sh
@@ -1,7 +1,8 @@
 #/bin/sh
 
 
-amd_gpu_tmp=$(cat /sys/class/hwmon/hwmon0/temp1_input)
+tmp_path=$(dirname $(rg amdgpu /sys/class/hwmon/hwmon*/name))
+amd_gpu_tmp=$(cat $tmp_path/temp1_input)
 amd_gpu_tmp=$(expr $(($amd_gpu_tmp / 1000)))
 echo "g_${amd_gpu_tmp}°C"
 
