b22de7451fc49bb6685bb6e40e1b6e20da512372
mandalorian <ssfwshutterbug@gmail.com>
2024-06-05 00:45:47 +0800

    good for now

--- scripts/change_wall.sh
+++ scripts/change_wall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # column after 7 are command executed (seperated by space)
-cmd=$(ps -ef | rg wl_wall | rg -v rg | choose 7:)
-pkill wl_wall && eval $cmd
+cmd=$(ps -ef | rg wl_wall | rg -v nvim | rg -v rg | choose 8:)
+eval $cmd
